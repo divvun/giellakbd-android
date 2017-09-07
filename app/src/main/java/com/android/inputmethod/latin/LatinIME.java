65c5e6353f720dfc2bf415c6c7cec7d0a56fbf1a
Brendan Molloy <brendan@bbqsrc.net>
2017-09-07 15:56:39 +0300

    Add basic support for Sentry

--- app/src/main/java/com/android/inputmethod/latin/LatinIME.java
+++ app/src/main/java/com/android/inputmethod/latin/LatinIME.java
@@ -109,6 +109,9 @@ import java.util.concurrent.TimeUnit;
 
 import javax.annotation.Nonnull;
 
+import io.sentry.Sentry;
+import io.sentry.android.AndroidSentryClientFactory;
+
 /**
  * Input method implementation for Qwerty'ish keyboard.
  */
@@ -560,6 +563,11 @@ public class LatinIME extends InputMethodService implements KeyboardActionListen
 
     @Override
     public void onCreate() {
+        final String sentryDsn = getString(R.string.sentry_dsn);
+        if (sentryDsn != null && !"".equals(sentryDsn)) {
+            Sentry.init(sentryDsn, new AndroidSentryClientFactory(getApplicationContext()));
+        }
+
         Settings.init(this);
         DebugFlags.init(PreferenceManager.getDefaultSharedPreferences(this));
         RichInputMethodManager.init(this);
