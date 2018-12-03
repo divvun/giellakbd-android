8aac17354cab74b73d926ab2d7fb5716b320770b
Brendan Molloy <brendan@technocreatives.com>
2018-12-03 18:15:21 +0100

    Revert all changes to setup wizard

--- app/src/main/java/no/divvun/Extensions.kt
+++ app/src/main/java/no/divvun/Extensions.kt
@@ -1,14 +1,5 @@
 package no.divvun
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavOptions
-import androidx.navigation.fragment.findNavController
-
 fun createTag(any: Any): String {
     return any.javaClass.simpleName.take(24)
-}
-
-fun Fragment.navigate(resId: Int, bundle: Bundle? = null, options: NavOptions? = null) {
-    findNavController().navigate(resId, bundle, options)
 }
