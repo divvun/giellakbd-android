a218f393447e894a4c831ba0058b03bf2e6ab1e2
Brendan Molloy <brendan@technocreatives.com>
2018-12-18 09:20:36 +0100

    Add Sentry logging for innocuous events

--- app/src/main/java/no/divvun/DivvunUtils.kt
+++ app/src/main/java/no/divvun/DivvunUtils.kt
@@ -3,6 +3,9 @@ package no.divvun
 import android.annotation.SuppressLint
 import android.content.Context
 import android.util.Log
+import io.sentry.Sentry
+import io.sentry.event.Event
+import io.sentry.event.EventBuilder
 import java.util.*
 import java.io.File
 import java.io.FileNotFoundException
@@ -30,8 +33,16 @@ object DivvunUtils {
 
     private fun getSpeller(fileName: String): DivvunSpell? {
         Log.d(TAG, "Loading dicts/$fileName")
+        Sentry.capture(EventBuilder()
+                .withMessage("Loading dicts/$fileName")
+                .withLevel(Event.Level.DEBUG)
+                .build())
         try {
             val inputStream = context.resources.assets.open("dicts/$fileName")
+            Sentry.capture(EventBuilder()
+                    .withMessage("Outputting file to ${context.filesDir.absolutePath}/$fileName")
+                    .withLevel(Event.Level.DEBUG)
+                    .build())
             Log.d(TAG, "Outputting file to ${context.filesDir.absolutePath}/$fileName")
             val outputStream = FileOutputStream(File(context.filesDir, fileName))
             inputStream.copyTo(outputStream)
@@ -39,10 +50,19 @@ object DivvunUtils {
             outputStream.flush()
             outputStream.close()
             Log.d(TAG, "$fileName should now exist in the file system")
+            Sentry.capture(EventBuilder()
+                    .withMessage("$fileName should now exist in the file system")
+                    .withLevel(Event.Level.DEBUG)
+                    .build())
         } catch (ex: FileNotFoundException) {
             return null
         }
 
-        return DivvunSpell("${context.filesDir.absolutePath}/$fileName")
+        return try {
+            DivvunSpell("${context.filesDir.absolutePath}/$fileName")
+        } catch (ex: Exception) {
+            Sentry.capture(ex)
+            null
+        }
     }
 }
