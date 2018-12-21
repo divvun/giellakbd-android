ad4fbbe11d2cb062aadd97d5b0330c8a44194369
Brendan Molloy <brendan@technocreatives.com>
2018-12-21 16:03:25 +0100

    Fix new phones having only two suggestions

--- app/src/main/java/no/divvun/dictionary/DivvunDictionary.kt
+++ app/src/main/java/no/divvun/dictionary/DivvunDictionary.kt
@@ -19,15 +19,22 @@ class DivvunDictionary(locale: Locale?): Dictionary(Dictionary.TYPE_MAIN, locale
 
     override fun getSuggestions(composedData: ComposedData, ngramContext: NgramContext, proximityInfoHandle: Long, settingsValuesForSuggestion: SettingsValuesForSuggestion, sessionId: Int, weightForLocale: Float, inOutWeightOfLangModelVsSpatialModel: FloatArray): ArrayList<SuggestedWords.SuggestedWordInfo> {
         val speller = this.speller ?: return ArrayList()
-        
-        val suggestions = speller.suggest(composedData.mTypedWord, N_BEST_SUGGESTION_SIZE, MAX_WEIGHT)
 
-        Log.d(tag, suggestions.toString())
+        val suggestions = mutableListOf(composedData.mTypedWord)
+        speller.suggest(composedData.mTypedWord, N_BEST_SUGGESTION_SIZE, MAX_WEIGHT).forEach {
+            suggestions.add(it)
+        }
 
         val result = suggestions.mapIndexed { index, suggestion ->
-            SuggestedWordInfo(suggestion, ngramContext.extractPrevWordsContext(),
-                    suggestions.size - index, SuggestedWordInfo.KIND_CORRECTION, this,
-                    SuggestedWordInfo.NOT_AN_INDEX, SuggestedWordInfo.NOT_A_CONFIDENCE)
+            if (index == 0) {
+                SuggestedWordInfo(suggestion, ngramContext.extractPrevWordsContext(),
+                        suggestions.size - index, SuggestedWordInfo.KIND_CORRECTION, this,
+                        SuggestedWordInfo.NOT_AN_INDEX, SuggestedWordInfo.NOT_A_CONFIDENCE)
+            } else {
+                SuggestedWordInfo(suggestion, ngramContext.extractPrevWordsContext(),
+                        SuggestedWordInfo.MAX_SCORE, SuggestedWordInfo.KIND_TYPED, this,
+                        SuggestedWordInfo.NOT_AN_INDEX, SuggestedWordInfo.NOT_A_CONFIDENCE)
+            }
         }
 
         return ArrayList(result)
