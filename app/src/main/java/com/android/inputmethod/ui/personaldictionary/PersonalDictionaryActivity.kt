6c23941d5873850d86203ce4eeedc2da416087cc
Dylan Hand <dylan.hand@gmail.com>
2025-08-12 13:20:58 +0200

    Handle edge-to-edge for User Dictionary view

--- app/src/main/java/com/android/inputmethod/ui/personaldictionary/PersonalDictionaryActivity.kt
+++ app/src/main/java/com/android/inputmethod/ui/personaldictionary/PersonalDictionaryActivity.kt
@@ -1,10 +1,12 @@
 package com.android.inputmethod.ui.personaldictionary
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.graphics.Insets
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import com.android.inputmethod.latin.databinding.ActivityPersonalDictionaryBinding
 
 
@@ -17,11 +19,37 @@ class PersonalDictionaryActivity : AppCompatActivity() {
         binding = ActivityPersonalDictionaryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Handle edge-to-edge display for Android 15+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            handleEdgeToEdge()
+        }
+
 //        host = binding.fDictionaryNavhost as NavHostFragment
 
-        val appBarConfiguration = AppBarConfiguration.Builder().build()
         setSupportActionBar(binding.tlDictionary)
 //        NavigationUI.setupActionBarWithNavController(this, host.navController, appBarConfiguration)
+    }
+
+    private fun handleEdgeToEdge() {
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { _, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            
+            binding.ablDictionary.setPadding(
+                binding.ablDictionary.paddingLeft,
+                systemBars.top,
+                binding.ablDictionary.paddingRight,
+                binding.ablDictionary.paddingBottom
+            )
+            
+            binding.fDictionaryNavhost.setPadding(
+                binding.fDictionaryNavhost.paddingLeft,
+                binding.fDictionaryNavhost.paddingTop,
+                binding.fDictionaryNavhost.paddingRight,
+                systemBars.bottom
+            )
+            
+            insets
+        }
     }
 
 //    override fun onSupportNavigateUp(): Boolean {
